229a64eed31865e57fbde08e554b7d9a169799ca
Jackson (Swimotter) Rubiano <mr.jacksonrubiano@gmail.com>
2025-11-04 18:36:09 -0500

    fix: Prevent REPD from looping forever

--- template.cpp
+++ template.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define REP(i, n) for (int i = 0; i < n; ++i)
-#define REPD(i, n) for (int i = n - 1; i >= 0; ++i)
+#define REPD(i, n) for (int i = n - 1; i >= 0; --i)
 #define FOR(i, a, b) for (int i = a; i < b; ++i)
 #define FORD(i, a, b) for (int i = a - 1; i >= b; --i)
 #define ALL(v) v.begin(), v.end()
